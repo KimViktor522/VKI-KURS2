47cbce1894cecd0b109677998556480bf65ec8db
KimVA <kimva@DOM201.LOCAL>
2020-02-20 16:09:41 +0700

    yehhh
    is end

--- Struct №1 Spisok(1svyaz).cpp
+++ Struct №1 Spisok(1svyaz).cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cstdio>
-#include <list>
 
 using namespace std;
 
@@ -13,6 +12,7 @@ struct List {
 	//число справа
 	Node* front{ nullptr };
 	Node* get_front() { return front; };
+	int listsize{};
 
 	//число слева
 	Node* back{ nullptr };
@@ -31,8 +31,10 @@ struct List {
 		current->valne = data;
 		current->next = front;
 		front = current;
+		++listsize;
 		return current;
 	}
+
 	//вставка числа влево
 	Node* push_back(int data) {
 		Node* current = new Node;
@@ -40,17 +42,26 @@ struct List {
 		current->valne = data;
 		back->next = current;
 		current->next = nullptr;
+		++listsize;
 		return current;
 	}
+
 	//вставка в определенную позицию 
 	Node* inst_in_pos(int position, int data) {
 		if (position == 1) {
 			return push_front(data);
 		}
+		if (position == listsize + 1) {
+			return push_back(data);
+		}
+		if (position > listsize + 1) {
+			cout << "NOT CORRECT!!!" << endl;
+			return front;
+		}
 		Node* current = new Node;
 		Node* currentOld{ get_front() };
 		int i{ 1 };
-		while (currentOld->next != nullptr) {			
+		while (true) {
 			if (i == position - 1) {
 				current->next = currentOld->next;
 			}
@@ -68,42 +79,57 @@ struct List {
 				break;
 			}
 			currentOld = currentOld->next;
-		}		
-		
+		}
+
 		return currentOld;
 	}
+
 	//удаление начала и конца
 	void del_front_and_back() {
+		if (!listsize) {
+			cout << "ERROR: DELETE FRONT AND BACK!!!" << endl;
+			return;
+		}
 		Node* current{ nullptr };
 		Node* currentF{ get_front() };
 		current = currentF->next;
+		--listsize;
+		if (!listsize) {
+			front = current;
+			cout << "Delete front" << endl << "ERROR: DELETE BACK!!!" << endl;
+			return;
+		}
 		Node* currentB{ current };
 		delete currentF;
-		Node* currentBuf{ nullptr };
 		while (currentB->next!=nullptr){
-			currentBuf = currentB->next;
-			if (currentBuf->next == nullptr) {
+			if (currentB->next->next == nullptr) {
 				currentB->next = nullptr;
 				break;
 			}
 			else currentB = currentB->next;
 		}
 		front = current;
-		delete currentBuf;
+		--listsize;
+		cout << "Delete front and back" << endl;
 	}
 
 	//удаление всего 
 	void clear() { 
+		if (!listsize) {
+			cout << "All clear" << endl;
+			return;
+		}
 		Node* current{ get_front() };
 		Node* currentDel{ nullptr };
-		while (current->next != nullptr){///переделать
+		while (current->next != nullptr){
 			currentDel = current->next;
-			current->valne = currentDel->valne;	
-			current = current->next;
+			delete current;	
+			current = currentDel;
 		}	
-		delete current;	
 		front = nullptr;
 		back = nullptr;
+		listsize = 0;
+		cout << "All clear" << endl;
 	}
 
 	//вывод
@@ -116,13 +142,22 @@ struct List {
 };
 
 int main() {
-	system("chcp 1251");
+	system("chcp 1251"); system("cls");
 	List list;
+	list.clear();
 	list.print();
 	list.push_front(5);
 	list.print();
 	list.push_front(4);
 	list.push_back(2);
+	list.print();
+	list.del_front_and_back();
+	list.print();
+	list.del_front_and_back();
+	list.print();
+	list.del_front_and_back();
+	list.clear();
+	list.print();
 	list.push_front(5);
 	list.push_back(6);
 	list.push_front(9);
@@ -138,6 +173,10 @@ int main() {
 	cin >> number;
 	list.inst_in_pos(position, number);
 
+	list.print();
+	list.clear();
+	list.push_front(2);
+	list.push_back(1);
 	list.print();
 	list.clear();
 	system("pause");
