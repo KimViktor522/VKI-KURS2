a866722125270a65b29f3329a5caf35303e3c240
KimViktor522 <KimViktor522@users.noreply.github.com>
2020-03-13 23:21:00 +0700

    добавил еще 1 способ решения

--- family/family/Source.cpp
+++ family/family/Source.cpp
@@ -78,6 +78,7 @@ void printMasFamily(mas& family, int N) {
 	cout << endl;
 }
 
+
 //построенеи таблицы приоритетов
 void unionTwoMasInOne(mas& priorityFamily, mas& man, mas& woman, int N) {
 	int number{}, j{};
@@ -114,32 +115,59 @@ void findFamily(mas& family, mas& priorityFamily, int N) {
 		family[numFamily][1] = l;
 		++numFamily;
 		for (int i{}; i < N; ++i) priorityFamily[i][l] = 0;
-		for (int j{}; j < N; ++j) priorityFamily[k][j] = 0;
+		for (int i{}; i < N; ++i) priorityFamily[k][i] = 0;
 	}
+}
+
+//второй способ опеделения пары
+void newFindFamily(mas& family, mas& man, mas& woman, int N) {
+	int num{};
+	for (int k{}; k < N; ++k) 
+		for (int j{}; j < N; ++j) {
+			for (int i{}; i < N; ++i) {
+				if (man[i][0] == -1) continue;
+				if (i == woman[man[i][k]][j]) {
+					family[num][0] = i;
+					family[num][1] = man[i][k];
+					++num;
+					for (int l{}; l < N; ++l) woman[man[i][k]][l] = -1;
+					for (int l{}; l < N; ++l) man[i][l] = -1;
+				}
+			}
+			if (num == N) return;
+		}
 }
 
 int main() {
 	system("chcp 1251"); system("chcp 65001"); system("cls");
 	srand((unsigned)time(0));
-	int N{}; //количество пар
+	int N{};	//количество пар
 	cout << "Введите количество пар: ";
 	cin >> N;
 	cout << endl;
 	mas man{}; creatMas(man, N); inputValunInMas(man, N);
 	mas woman{}; creatMas(woman, N); inputValunInMas(woman, N);
-	mas priorityFamily{}; creatMas(priorityFamily, N);
 	cout << "man" << endl;
 	printMas(man, N);
 	cout << "woman" << endl;
 	printMas(woman, N);
+	cout << "1 способ: " << endl;
+	mas priorityFamily{}; creatMas(priorityFamily, N);
+	mas family{}; creatMasF(family, N);
 	unionTwoMasInOne(priorityFamily, man, woman, N);
 	cout << endl << "priority family" << endl;
 	printMasPF(priorityFamily, N);
-	mas family{}; creatMasF(family, N);
 	findFamily(family, priorityFamily, N);
 	cout << "family" << endl;
+	printMasFamily(family, N); 
+	cout << "2 способ: " << endl;	
+	newFindFamily(family, man, woman, N);
+	cout << "man" << endl;
+	printMas(man, N);
+	cout << "woman" << endl;
+	printMas(woman, N);
+	cout << "family" << endl;
 	printMasFamily(family, N);
-
 	system("pause");
 	return 0;
 }
